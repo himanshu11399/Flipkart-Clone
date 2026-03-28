9028ca662694d6644cea1907a1ba9527f51cc39d
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-03-29 01:46:30 +0530

    first commit

--- backend/scripts/check_products.js
+++ backend/scripts/check_products.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.join(__dirname, '../backend/.env') });
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const sequelize = new Sequelize(process.env.DB_URI, {
   dialect: 'postgres',
